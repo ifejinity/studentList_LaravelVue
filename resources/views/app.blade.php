8218e8510533dd30d42294f6b81e2805a5fb0c05
ifejinity <mrlonzanida08@gmail.com>
2023-09-20 23:38:29 +0800

    9/20/23

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,7 @@
         <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js" defer></script>
         {{-- jquery --}}
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 
         @vite('resources/css/app.css')
         @vite('resources/js/app.js')

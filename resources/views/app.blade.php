01a7952a7a222e9681e0a92b1131f0d8c63d28a5
ifejinity <mrlonzanida08@gmail.com>
2023-09-21 14:11:06 +0800

    9/21/23

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,15 +3,7 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-        {{-- sweet alert --}}
-        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11" defer></script>
-        {{-- toastfiy --}}
-        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
-        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js" defer></script>
-        {{-- jquery --}}
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-
+        @include('partials.__cdn')
         @vite('resources/css/app.css')
         @vite('resources/js/app.js')
         @inertiaHead

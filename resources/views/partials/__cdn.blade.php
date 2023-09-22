2d9f6b2c70d11ddfee8a93f893e05a1591f8a4f5
ifejinity <mrlonzanida08@gmail.com>
2023-09-22 13:45:23 +0800

    9/22/23

--- resources/views/partials/__cdn.blade.php
+++ resources/views/partials/__cdn.blade.php
@@ -3,3 +3,5 @@
 {{-- toastfiy --}}
 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js" defer></script>
+{{-- bootstrap icon --}}
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
